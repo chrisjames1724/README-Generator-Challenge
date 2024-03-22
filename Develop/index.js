8104ee568c95ceaef64c523b5945a40f56c8b789
chrisjames1724 <christopher.cummings28@gmail.com>
2024-03-21 20:46:18 -0600

    starter clone commit

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    message: "" ,
+    message: "" ,
+    message: "" ,
+    message: "" ,
+    message: "" ,
+}
+];
+
+// TODO: Create a function to write README file
+function writeToFile(README, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
